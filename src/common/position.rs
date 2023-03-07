8f7677d512e1a80366586a75216e66ff74bb977f
Chia-Hsiang Cheng <gary.chiahsiang.cheng@gmail.com>
2023-03-07 17:57:43 +0800

    Add tests for Position and PositionRange

--- src/common/position.rs
+++ src/common/position.rs
@@ -39,3 +39,70 @@ impl ToString for PositionRange {
         )
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{Position, PositionRange};
+
+    #[test]
+    fn test_position_new() {
+        let pos = Position::new();
+        assert_eq!(pos.line, 1);
+        assert_eq!(pos.column, 1);
+        assert_eq!(pos.index, 0);
+    }
+
+    #[test]
+    fn test_position_to_string() {
+        let pos = Position {
+            line: 13,
+            column: 23,
+            index: 50,
+        };
+        assert_eq!(
+            pos.to_string(),
+            format!(
+                "(Line: {}, Column: {}, Index: {})",
+                pos.line, pos.column, pos.index
+            )
+        );
+        let pos = Position {
+            line: 812,
+            column: 343,
+            index: 903,
+        };
+        assert_eq!(
+            pos.to_string(),
+            format!(
+                "(Line: {}, Column: {}, Index: {})",
+                pos.line, pos.column, pos.index
+            )
+        );
+    }
+
+    #[test]
+    fn test_position_range_to_string() {
+        let pos1 = Position {
+            line: 32,
+            column: 43,
+            index: 103,
+        };
+        let pos2 = Position {
+            line: 90,
+            column: 18,
+            index: 190,
+        };
+        let range = PositionRange {
+            start: pos1,
+            end: pos2,
+        };
+        assert_eq!(
+            range.to_string(),
+            format!(
+                "{{Start: {}, End: {}}}",
+                range.start.to_string(),
+                range.end.to_string()
+            )
+        );
+    }
+}
